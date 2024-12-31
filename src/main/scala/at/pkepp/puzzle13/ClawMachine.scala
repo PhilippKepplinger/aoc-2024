5c6209dcba50baea1c859d7b9d7b8bb7651ab99e
Philipp Kepplinger <philipp.kepplinger@outlook.at>
2024-12-31 14:32:19 +0100

    Simplify Code

--- src/main/scala/at/pkepp/puzzle13/ClawMachine.scala
+++ src/main/scala/at/pkepp/puzzle13/ClawMachine.scala
@@ -11,7 +11,7 @@ class ClawMachine(val a: Vector,
   val costB = 1
 
   def tokens(): BigDecimal = {
-    val buttons = getButtons()
+    val buttons = getButtons
     val solvable = isSolvable(buttons)
 
     if solvable
@@ -19,8 +19,8 @@ class ClawMachine(val a: Vector,
     else BigDecimal.exact(0)
   }
 
-  private def getButtons(): Buttons = {
-    val vB = getB()
+  private def getButtons: Buttons = {
+    val vB = getB
     new Buttons(getAFromB(vB), vB)
   }
 
@@ -28,7 +28,7 @@ class ClawMachine(val a: Vector,
     (P.y / a.y) - vB * (b.y / a.y)
   }
 
-  private def getB(): BigDecimal = {
+  private def getB: BigDecimal = {
     (P.x * a.y - a.x * P.y) / (b.x * a.y - b.y * a.x)
   }
 
